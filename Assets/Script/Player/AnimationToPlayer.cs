b90f379a8dc94ba052a7b312d091c18ae7a34ce1
pongsatornthn-art <pongsatorn.thn@spumail.net>
2026-04-08 11:11:15 +0700

    Up39

---
+++ Assets/Script/Player/AnimationToPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AnimationToPlayer : MonoBehaviour
+{
+    private PlayerController player;
+
+    void Start()
+    {
+        // คำสั่งนี้จะวิ่งไปหา PlayerController ที่แปะอยู่บนตัวแม่
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    // ⭐ ฟังก์ชันนี้แหละที่เราจะเอาไปใส่ใน Animation Event!
+    public void TriggerDamageEvent()
+    {
+        if (player != null)
+        {
+            // สั่งให้ตัวแม่ทำการคำนวณดาเมจ
+            player.DealDamage();
+        }
+    }
+}
